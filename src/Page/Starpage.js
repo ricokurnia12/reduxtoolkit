909ad705313d80a5e5f099818004caacc3d0549f
bafir24 <bafirgenius@gmail.com>
2022-10-27 15:54:10 +0700

    tambah foto starpage

--- src/Page/Starpage.js
+++ src/Page/Starpage.js
@@ -1,13 +1,25 @@
 import React from "react";
-import Starpagestyle from './Starpagestyle.css'
+import Starpagestyle from "./Starpagestyle.css";
+import Peoplestarpage from "../Assets/Peoplestarpage.png";
 
 const Starpage = () => {
     return (
-        <div className="container ms-5">
-            <p>Halo Kiki,</p>
-            <p>Selamat Datang!</p>
-            <h3>TES MBTI</h3>
-            <button className="buttonmulai">Mulai Tes</button>
+        <div className="container ms-5 mb-15">
+            <div className="hello">
+                <p>Halo Kiki,</p>
+                <p>Selamat Datang!</p>
+                <h3>TES MBTI</h3>
+                <button className="buttonmulai">Mulai Tes</button>
+                <div className="position-absolute top-50 end-0 translate-middle-y">
+                    <div className="container">
+                        <img
+                            src={Peoplestarpage}
+                            width={683}
+                            height={653}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
